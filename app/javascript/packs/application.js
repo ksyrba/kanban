46f1cfe3d7649d8a93f493a200a1c833854d9268
ksyrba <ksyrbaba0812@gmail.com>
2022-07-04 03:25:35 +0000

    introduction bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "jquery";
+import "popper.js";
 
 Rails.start()
 Turbolinks.start()
