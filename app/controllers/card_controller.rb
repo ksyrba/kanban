adb5dc15129cf9196f4017d5584489fb79a3d4f0
ksyrba <ksyrbaba0812@gmail.com>
2022-07-05 11:40:04 +0000

    create card destroy

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -1,11 +1,11 @@
 class CardController < ApplicationController
-  before_action :set_card, only: %i(show edit update)
-  
+  before_action :set_card, only: %i(show edit update destroy)
+
   def new
     @card = Card.new
     @list = List.find_by(id: params[:list_id])
   end
-  
+
   def create
     @card = Card.new(card_params)
     if @card.save
@@ -14,10 +14,10 @@ class CardController < ApplicationController
       render action: :new
     end
   end
-  
+
   def show
   end
-  
+
   def edit
   end
 
@@ -29,13 +29,17 @@ class CardController < ApplicationController
     end
   end
 
+  def destroy
+    @card.destroy
+    redirect_to :root
+  end
+
   private
-  
-  def card_params
-    params.require(:card).permit(:title, :memo, :list_id)
-  end
-  
-  def set_card
-    @card = Card.find_by(id: params[:id])
-  end
+    def card_params
+      params.require(:card).permit(:title, :memo, :list_id)
+    end
+
+    def set_card
+      @card = Card.find_by(id: params[:id])
+    end
 end
