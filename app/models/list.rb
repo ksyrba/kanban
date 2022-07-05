325755c39b707267ea4f9736862a641c749a3a9e
ksyrba <ksyrbaba0812@gmail.com>
2022-07-05 09:42:13 +0000

    create card model

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,5 @@
 class List < ApplicationRecord
+ has_many :cards, dependent: :destroy    
+    
   validates :title, length: { in: 1..255 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_03_074412) do
+ActiveRecord::Schema.define(version: 2022_07_05_093710) do
+
+  create_table "cards", force: :cascade do |t|
+    t.string "title", limit: 255, null: false
+    t.text "memo", limit: 1000
+    t.integer "list_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["list_id"], name: "index_cards_on_list_id"
+  end
 
   create_table "lists", force: :cascade do |t|
     t.string "title", null: false
@@ -18,4 +27,5 @@ ActiveRecord::Schema.define(version: 2022_07_03_074412) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "cards", "lists"
 end
