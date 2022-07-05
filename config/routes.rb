ce1553bdf34ac562c83eb34059bd90d0aadd0ec7
ksyrba <ksyrbaba0812@gmail.com>
2022-07-05 10:14:26 +0000

    create card new

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'card/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'top#index'
-  resources :list, only: %i(new create edit update destroy)
+  resources :list, only: %i(new create edit update destroy) do
+    resources :card, only: %i(new create)
+  end
 end
